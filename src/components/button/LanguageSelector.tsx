6cb7d48c7eeee59be549fdfb540a1d6c386d3880
antoniussinaga-dev <antoniussinaga.dev@gmail.com>
2026-03-06 06:20:04 +0700

    update popover language selector

--- src/components/button/LanguageSelector.tsx
+++ src/components/button/LanguageSelector.tsx
@@ -3,16 +3,85 @@
 import { useState, MouseEvent, useCallback } from 'react';
 import {
     Button,
-    Menu,
-    MenuItem,
     Box,
     Typography,
-    ListItemIcon,
+    ClickAwayListener,
+    Dialog,
+    DialogContent,
+    Grid,
+    Paper,
+    Popper,
 } from '@mui/material';
 import { useI18n, languages } from "@/i18n";
 import { defaultLanguage, Language } from "@/i18n/config";
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
+
+function DialogLanguageSelector() {
+    return (
+        <Dialog open fullWidth maxWidth="md">
+            <DialogContent sx={{ p: 3 }}>
+                <Grid container spacing={2}>
+                    {languages.map((item, i) => (
+                        <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={i} sx={{ display: "flex" }}>
+                            <Box
+                                sx={{
+                                    p: 2,
+                                    width: "100%",
+                                    borderRadius: 2,
+                                    border: "1px solid",
+                                    borderColor: "divider",
+                                    bgcolor: "background.paper",
+                                    cursor: "pointer",
+                                    transition: "all .15s ease",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    gap: 1.5,
+
+                                    "&:hover": {
+                                        borderColor: "primary.main",
+                                        transform: "translateY(-2px)",
+                                        boxShadow: 3,
+                                    }
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        gap: 1.5
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            fontSize: "1.8rem",
+                                            lineHeight: 1
+                                        }}
+                                    >
+                                        {item.flag}
+                                    </Box>
+
+                                    <Typography fontWeight={700} fontSize="1.2rem">
+                                        {item.code}
+                                    </Typography>
+                                </Box>
+
+                                <Box>
+                                    <Typography fontSize="0.9rem" color="text.secondary">
+                                        {item.englishName}
+                                    </Typography>
+                                    <Typography fontWeight={500}>
+                                        {item.nativeName}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid>
+            </DialogContent>
+        </Dialog>
+    );
+}
 
 export function ButtonLanguageSelector() {
     const router = useRouter();
@@ -24,7 +93,6 @@ export function ButtonLanguageSelector() {
     const language = languages.find(item => (item.code == locale)) || defaultLanguage;
     const open = Boolean(anchorEl);
 
-    // languages.find(item => (item.code == locale))
 
     const handleOpen = (event: MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
@@ -52,6 +120,7 @@ export function ButtonLanguageSelector() {
 
     return (
         <Box>
+            {/* <DialogLanguageSelector /> */}
             <Button
                 id="btn-language"
                 onClick={handleOpen}
@@ -75,43 +144,65 @@ export function ButtonLanguageSelector() {
                     {language.code.toUpperCase()}
                 </Typography>
             </Button>
-
-            <Menu
-                id="language-menu"
-                anchorEl={anchorEl}
+            <Popper
                 open={open}
-                onClose={handleClose}
-                // anchorOrigin={{
-                //     vertical: 'bottom',
-                //     horizontal: 'left',
-                // }}
-                // transformOrigin={{
-                //     vertical: 'top',
-                //     horizontal: 'left',
-                // }}
-                sx={{
-                    mt: 1,
-                }}
-                slotProps={{
-                    paper: {
-                        sx: {
-                            maxHeight: 300,
-                            overflowY: "auto",
+                anchorEl={anchorEl}
+                placement="bottom-end"
+                modifiers={[
+                    {
+                        name: "offset",
+                        options: {
+                            offset: [0, 12],
                         },
-                    }
-                }}
+                    },
+                ]}
             >
-                {
-                    languages.map((item, i) => (
-                        <MenuItem onClick={() => handleSelect(item)} key={i}>
-                            <ListItemIcon>
-                                {item.flag || item.code}
-                            </ListItemIcon>
-                            {item.nativeName}
-                        </MenuItem>
-                    ))
-                }
-            </Menu>
+                <ClickAwayListener onClickAway={handleClose}>
+                    <Paper
+                        sx={{
+                            width: 640,
+                            maxHeight: 420,
+                            overflowY: "auto",
+                            p: 2,
+                            borderRadius: 2
+                        }}
+                    >
+                        <Grid container spacing={2}>
+                            {languages.map((item, i) => (
+                                <Grid size={{ xs: 3 }} key={i}>
+                                    <Box
+                                        onClick={() => handleSelect(item)}
+                                        sx={{
+                                            textAlign: "center",
+                                            p: 1,
+                                            borderRadius: 1,
+                                            cursor: "pointer",
+                                            transition: "all .15s",
+
+                                            "&:hover": {
+                                                bgcolor: "action.hover"
+                                            }
+                                        }}
+                                    >
+                                        <Box
+                                            sx={{
+                                                fontSize: "1.8rem",
+                                                mb: 0.5
+                                            }}
+                                        >
+                                            {item.flag}
+                                        </Box>
+
+                                        <Typography fontSize="0.8rem">
+                                            {item.nativeName}
+                                        </Typography>
+                                    </Box>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Paper>
+                </ClickAwayListener>
+            </Popper>
         </Box>
     );
 };
